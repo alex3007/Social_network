dc2b0d6e5d893c0d6d1768c17ed7c4d343f6ec1c
alex3007 <alex300785@tut.by>
2020-07-06 15:18:06 +0300

    updated 06-07-2020

---
+++ precache-manifest.07e96f76d574e52ffbe2e43865526580.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f7a69d5aa6bfa5aeb1440c712295f6",
+    "url": "/Social_network/index.html"
+  },
+  {
+    "revision": "4487b2ed4acd436e4066",
+    "url": "/Social_network/static/css/3.16a845df.chunk.css"
+  },
+  {
+    "revision": "43cad64be26b16695c1e",
+    "url": "/Social_network/static/css/4.10e1bed3.chunk.css"
+  },
+  {
+    "revision": "26525e5f92f12f80c694",
+    "url": "/Social_network/static/css/main.7532b8f8.chunk.css"
+  },
+  {
+    "revision": "ed47e7bf2bda4b736472",
+    "url": "/Social_network/static/js/2.5e0830bb.chunk.js"
+  },
+  {
+    "revision": "4487b2ed4acd436e4066",
+    "url": "/Social_network/static/js/3.2c67e598.chunk.js"
+  },
+  {
+    "revision": "43cad64be26b16695c1e",
+    "url": "/Social_network/static/js/4.f3ca2084.chunk.js"
+  },
+  {
+    "revision": "26525e5f92f12f80c694",
+    "url": "/Social_network/static/js/main.dc00477b.chunk.js"
+  },
+  {
+    "revision": "87f85e8aa0cd516b3a1f",
+    "url": "/Social_network/static/js/runtime~main.b437b864.js"
+  },
+  {
+    "revision": "595172f9bbf8352da92accece4ddbfe9",
+    "url": "/Social_network/static/media/friend.595172f9.jpg"
+  },
+  {
+    "revision": "b8c9a73ee008a27a3f8a25ecf07afdde",
+    "url": "/Social_network/static/media/intro.b8c9a73e.png"
+  },
+  {
+    "revision": "f0fb1b3492fb2509e4778d7f0cda4d80",
+    "url": "/Social_network/static/media/message.f0fb1b34.png"
+  },
+  {
+    "revision": "ba0c4a8e9ba9b56e09d5382f81ba8020",
+    "url": "/Social_network/static/media/myphoto.ba0c4a8e.jpg"
+  },
+  {
+    "revision": "9044b20ba9e172e33e192a148863925a",
+    "url": "/Social_network/static/media/spinning-circles.9044b20b.svg"
+  },
+  {
+    "revision": "3903a2af2055cf100c53ceee4a57b62d",
+    "url": "/Social_network/static/media/titlephoto.3903a2af.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social_network/precache-manifest.7e8a72461212951d36ab8f4802e0e07d.js"
+  "/Social_network/precache-manifest.07e96f76d574e52ffbe2e43865526580.js"
 );
 
 self.addEventListener('message', (event) => {
